dc6882ae465d36c96aa906dfcb4706f97ccbc5dc
SrinuKatama <59701542+SrinuKatama@users.noreply.github.com>
2020-05-25 12:58:59 +0530

    [srinu]Configaration file modified

--- fundoo-note-service/src/main/java/com/example/demo/configaration/Config.java
+++ fundoo-note-service/src/main/java/com/example/demo/configaration/Config.java
@@ -2,8 +2,10 @@ package com.example.demo.configaration;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class Config
 {
 	@Bean
